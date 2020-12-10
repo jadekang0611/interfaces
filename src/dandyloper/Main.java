4916855e64895b4cd05cf312773ad1e345fc1c31
jadekang <jkang.se@gmail.com>
2020-12-10 01:05:13 -0800

    Studying interfaces in Java

---
+++ src/dandyloper/Main.java
@@ -0,0 +1,8 @@
+package dandyloper;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/dandyloper/Person.java
@@ -0,0 +1,11 @@
+package dandyloper;
+
+public class Person {
+    String name;
+    Person (String name) {
+        this.name = name;
+    }
+    String chat () {
+        return "Hi, I'm a person!";
+    }
+}
